df1f766fa0e748d5eb1ac7170779b4f27f07caae
Sahab-Mohamed <sahab@Sigma.localdomain>
2025-12-27 15:32:53 -0500

    adding required arguments information in run.py~

--- main.py
+++ main.py
@@ -62,7 +62,7 @@ def go(config: DictConfig):
             _ = mlflow.run(
                 f"{config['main']['components_repository']}/data_check",
                 "main",
-                env_manager="conda",
+                env_manager="local",
                 parameters={
                     "csv": "clean_sample.csv:latest",
                     "ref": "clean_sample.csv:reference",
